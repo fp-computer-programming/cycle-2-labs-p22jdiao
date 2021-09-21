468606340ccc68a0bf2eba0124351f16307ea50c
p22jdiao <p22jdiao@fairfieldprep.org>
2021-09-21 10:05:51 -0400

    6-1

---
+++ lab_6-1.py
@@ -0,0 +1,11 @@
+# Author: JD 09/21/2021
+
+first_name = input("Please enter your first name: ")
+
+last_name = input("Please enter your last name: ")
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+#print(first_name, last_name)
